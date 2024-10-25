ad358d856ab448f1e47cda75aad9d92b1f890e98
farhan-navas <farhanmnavas@gmail.com>
2024-10-26 04:33:59 +0800

    halfway thru non-losing moves, test new conv state to board function

--- board.py
+++ board.py
@@ -10,6 +10,9 @@ class Board:
         self.counter = 0
         self.moves = [0] * 42
         self.player1, self.player2 = self.convert_state_to_board(state)
+        self.bottom_mask = 0b1000000100000010000001000000100000010000001
+        self.board_mask =  0b0111111011111101111110111111011111101111110111111
+
 
     def convert_state_to_board(self, state):
         # p1 -> represents the position of player 1 tokens
@@ -48,7 +51,7 @@ class Board:
         p2 = '0' + p2
 
         return int(p1, 2), int(p2, 2)
-    
+
     # TEMP FUNCTIONS
 
     def eval_func(self):
@@ -121,76 +124,107 @@ class Board:
         moves.sort(key=lambda x: abs(x - 3))
         return moves
     
-    def bits_to_cols(self, b):
-        cols = set()
-        while b:
-            lsb = b & -b
-            indx = lsb.bit_length() - 1
-            c = indx // 7
-            cols.add(c)
-            b &= b - 1
-        return cols
+    def bits_to_moves(self, b):
+        moves = []
+        for c in range(7):
+            if self.height[c] >= (c + 1) * 7:
+                continue
+            idx = self.height[c]
+            if b & (1 << idx):
+                moves.append(c)
+
+        moves.sort(key=lambda x: abs(x - 3)) 
+        return moves
     
     def possible(self):
         return (self.player1 | self.player2) ^ ((self.player1 | self.player2) + self.bottom_mask)
 
     def compute_winning_positions(self):
-        empty = ~(self.player1 | self.player2)
-        r = 0
+        # vertical
+        # print(bin(self.player2))
+        # self.print_bitboard(self.player2)
+        w = (self.player2 >> 1) & (self.player2 >> 2) & (self.player2 >> 3)
+        # print("self.p2")
+        # self.print_bitboard(self.player2)
+        # print("w")
+        # self.print_bitboard(w)
 
-        # Vertical
-        m = self.player2 & (self.player2 << 1) & (self.player2 << 2)
-        r |= (m << 3) & empty
+        # horizontal
+        curr = (self.player2 << 7) & (self.player2 << 14)
+        w |= curr & (self.player2 << 21)
+        w |= curr & (self.player2 >> 7)
+        curr >>= 21
+        w |= curr & (self.player2 << 7)
+        w |= curr & (self.player2 >> 21)
+        # print("w horizontal")
+        # self.print_bitboard(w)
 
-        # Horizontal
-        m = self.player2 & (self.player2 << 7)
-        r |= ((m & (self.player2 << 14)) << 21) & empty
-        r |= ((m & (self.player2 >> 7)) << 7) & empty
-        m = self.player2 & (self.player2 >> 7)
-        r |= ((m & (self.player2 >> 14)) >> 21) & empty
-        r |= ((m & (self.player2 << 7)) >> 7) & empty
+        # diagonal 1
+        curr = (self.player2 << 6) & (self.player2 << 12)
+        w |= curr & (self.player2 << 18)
+        w |= curr & (self.player2 >> 6)
+        curr >>= 18
+        w |= curr & (self.player2 << 6)
+        w |= curr & (self.player2 >> 18)
 
-        # Diagonal /
-        m = self.player2 & (self.player2 << 6)
-        r |= ((m & (self.player2 << 12)) << 18) & empty
-        r |= ((m & (self.player2 >> 6)) << 6) & empty
-        m = self.player2 & (self.player2 >> 6)
-        r |= ((m & (self.player2 >> 12)) >> 18) & empty
-        r |= ((m & (self.player2 << 6)) >> 6) & empty
+        # diagonal 2
+        curr = (self.player2 << 8) & (self.player2 << 16)
+        w |= curr & (self.player2 << 24)
+        w |= curr & (self.player2 >> 8)
+        curr >>= 24
+        w |= curr & (self.player2 << 8)
+        w |= curr & (self.player2 >> 24)
 
-        # Diagonal \
-        m = self.player2 & (self.player2 << 8)
-        r |= ((m & (self.player2 << 16)) << 24) & empty
-        r |= ((m & (self.player2 >> 8)) << 8) & empty
-        m = self.player2 & (self.player2 >> 8)
-        r |= ((m & (self.player2 >> 16)) >> 24) & empty
-        r |= ((m & (self.player2 << 8)) >> 8) & empty
-
-        return r
+        return w & self.board_mask
 
     def non_losing_moves(self):
         possible = self.possible()
         opp_win = self.compute_winning_positions()
-        forced = possible & opp_win
+        # forced = self.bottom_mask & opp_win
+
+        next_possible = possible ^ (self.player1 | self.player2)
+        forced = next_possible & opp_win
+        
+        # print("next_possible: ")
+        # self.print_bitboard(next_possible)
+        # print("opp win: ")
+        # self.print_bitboard(opp_win)
 
         if forced:
             if forced & (forced - 1):
-                return 0
+                return []
             else:
                 possible = forced
+                moves = []
+                for col in range(7):
+                    for row in range(7):
+                        b = 1 << (col * 7 + row)
+                        if b & possible:
+                            moves.append(col)
+                return moves
 
         else:
+            # print("error here 3")
+            # print("opp_win >> 1")
+            # self.print_bitboard(opp_win >> 1)
             possible &= ~(opp_win >> 1)
 
-        return possible
+        # print("2nd possible: ")
+        # self.print_bitboard(possible)
+        return self.bits_to_moves(possible)
+    
+    def pop_count(self, board):
+        board &= self.board_mask
+        return bin(board).count('1') 
 
-    def print_bitboard_p1(self):
+    def print_bitboard(self, b):
+        print(bin(b))
         top_level = [6, 13, 20, 27, 34, 41, 48]
         p_board = [[0] * 7 for _ in range(6)]
-        str_board = str(bin(self.player1))[2:]
+        str_board = str(bin(b))[2:]
         pad_zeros = 48 - len(str_board)
         str_board = pad_zeros * '0' + str_board
-        print(str_board)
+        print("board in binary: ", str_board)
 
         for i in range(48):
             if (i % 7) in top_level:
@@ -201,28 +235,6 @@ class Board:
                 c = i % 7
                 p_board[c][r] = str_board[i]
         
-        for row in p_board:
-            row.reverse()
-
-        print(*p_board, sep="\n")
-
-    def print_bitboard_p2(self):
-        top_level = [6, 13, 20, 27, 34, 41, 48] 
-        p_board = [[0] * 7 for _ in range(6)]
-        str_board = str(bin(self.player2))[2:]
-        pad_zeros = 48 - len(str_board)
-        str_board = pad_zeros * '0' + str_board
-        print(str_board)
-
-        for i in range(42):
-            if (i % 7) in top_level:
-                continue
-            
-            else:
-                r = i // 7
-                c = i % 7
-                p_board[c][r] = str_board[i]
-
         for row in p_board:
             row.reverse()
 
--- testing.py
+++ testing.py
@@ -1,45 +1,36 @@
 from board import Board
 
+arr1 = [
+    [0,0,0,0,0,0,0],  # Row 0 (Top)
+    [0,0,0,0,0,0,0],  # Row 1
+    [0,0,0,0,0,0,0],  # Row 2
+    [0,0,2,2,2,0,0],  # Row 3
+    [0,0,1,2,1,0,0],  # Row 4
+    [0,2,1,1,1,0,0],  # Row 5 (Bottom)
+]
 
-empty = [[0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0],
-         ]
-
-arr1 = [[0,0,0,0,0,0,0],
+arr2 = [[0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0],
-        [0,0,0,1,0,0,0],
-        [0,0,1,2,2,2,0],
-        [0,1,1,2,1,1,2],
-        ]
-
-arr2 = [[0,0,0,2,0,0,0],
         [0,0,0,2,0,0,0],
-        [0,0,0,1,0,0,0],
-        [0,0,0,1,0,0,0],
-        [0,0,0,2,2,0,0],
-        [0,0,1,2,1,1,0],
+        [1,0,0,2,2,2,0],
+        [1,1,0,2,1,1,0],
         ]
 
-b = Board(arr1)
+b = Board(arr2)
 print(f"player 1: {b.player1}, player 2: {b.player2}, height: {b.height}, counter: {b.counter}")
-print(b.gen_valid_moves())
-b.print_bitboard_p1()
-b.print_bitboard_p2()
+# b.print_bitboard(b.player1)
+# b.print_bitboard(b.player2)
+print(b.height)
+print("valid moves: ", b.gen_valid_moves())
+print("non-losing moves: ", b.non_losing_moves())
 
-# b = Board(arr2)
+# b = Board(arr1)
 # print(f"player 1: {b.player1}, player 2: {b.player2}, height: {b.height}, counter: {b.counter}")
-# print(b.height)
-# b.print_bitboard_p1()
-# b.print_bitboard_p2()
+# print("valid moves: ", b.gen_valid_moves())
+# print("non-losing moves: ", b.non_losing_moves())
 
 # b = Board(empty)
 # # print(b.player1, b.player2)
 # print(b.height)
 # print(b.gen_valid_moves())
-# # b.print_bitboard_p1()
-# # b.print_bitboard_p2()
